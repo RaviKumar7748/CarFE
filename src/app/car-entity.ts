ba23a2f179de7d5beea7b0426b896c3cb05a3784
ravi <mrravigupta51>
2023-03-27 17:28:11 +0530

    Intial Commit

---
+++ src/app/car-entity.ts
@@ -0,0 +1,13 @@
+export class CarEntity 
+{
+    regn_No:String;
+    car_Company:String;
+    car_Model:String;
+    year_Of_Manufacturing:String;
+    owner_Name:String;
+constructor(  )
+
+    {
+
+    }
+}
---
+++ src/app/service/carservice.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CarEntity } from '../car-entity';
+@Injectable({
+  providedIn: 'root'
+})
+export class CarserviceService {
+
+  public id:string;
+
+  private url : string  ="http://localhost:8084/car";
+  constructor(private http:HttpClient) { }
+  
+  //Show All Cars
+
+  getAllCars(): Observable<CarEntity[]>{
+    return this.http.get<CarEntity[]>(`${this.url}/all`);
+  }
+
+  //Add Car
+
+  addAllCars(carEntity:CarEntity): Observable<CarEntity[]>{
+    return this.http.post<CarEntity[]>(`${this.url}/add`,carEntity);
+  }
+
+  updateAllCars(carEntity:CarEntity): Observable<CarEntity>{
+    return this.http.put<CarEntity>(`${this.url}/update`,carEntity);
+  }
+
+
+  getCarById(id:string): Observable<CarEntity>{
+    return this.http.get<CarEntity>(`${this.url}/one/${id}`);
+  }
+
+  deleteCarById(id:string): Observable<CarEntity>{
+    return this.http.delete<CarEntity>(`${this.url}/delete/${id}`);
+  }
+
+}
